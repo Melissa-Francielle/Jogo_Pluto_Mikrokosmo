3eb44d335cf0f34d849b90cda8060a59256aa802
Gustav Keller <gustavkeller2708@gmail.com>
2024-09-23 13:13:52 -0300

    Adicionando o jogo de apresentação para completar

---
+++ PlutoMikrokosmos/scripts/start.gd
@@ -0,0 +1,12 @@
+extends Node
+
+
+
+func _ready() -> void:
+	$text_score/Label.text = str(Game.bestscore)
+
+# -----------------------------------------------------
+# BOTÃO DE JOGAR E GUIA E SAIR
+
+
+
